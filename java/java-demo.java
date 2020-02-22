34581b7daf080655334db48bf1ae4925b587d41c
passpay <61136878+passpay@users.noreply.github.com>
2020-02-22 12:12:40 +0800

    update

--- java/java-demo.java
+++ java/java-demo.java
@@ -1,3 +1,4 @@
+package com.olain.java8.默认方法zcatpaydemo;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -24,16 +25,17 @@ import java.time.Instant;
  */
 public class JavaDemo {
 
-    private static final String HOST = "/cat-pay/open/order";
+    //联系客服获取商户, 密钥与域名
+    private static final String MERCHANT_NO = "";
     private static final String KEY = "";
 
     public static void main(String[] args) {
         //查询
         //queryOrder();
-        //创建
+        //创建, 页面跳转形式
         //createOrder();
         //回调
-        callback();
+        //callback();
     }
 
 
@@ -42,18 +44,20 @@ public class JavaDemo {
      */
     public static void createOrder() {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("orderNo", "123456789000");
-        jsonObject.put("merchantNo", "20200113185052721173545318");
+        jsonObject.put("orderNo", "123456789004");
+        jsonObject.put("merchantNo", MERCHANT_NO);
         jsonObject.put("amount", "100");
         jsonObject.put("payMode", "ebank");
         jsonObject.put("notifyUrl", "https://www.baidu.com/");
         jsonObject.put("returnUrl", "https://www.baidu.com/");
         jsonObject.put("ts", Instant.now().getEpochSecond());
-        String signReduce = generateSignReduce(jsonObject, KEY);
-        String sign = encodeMD5(signReduce);
+        String signReduce = generateSignReduce(jsonObject);
+        String sign = encodeMD5(signReduce + "&key=" + KEY);
         jsonObject.put("sign", sign);
 
-        String s = jsonPost(jsonObject.toJSONString(), HOST);
+        //链接跳转形式, 域名请联系客服
+        //host + /#/create?amount=100&merchantNo=20200113185052721173545318&notifyUrl=baidu.com&orderNo=1582342812127&payMode=ebank&returnUrl=baidu.com&ts=1582342812&sign=126a8a129faff7d71a1e2208f5c938fb
+        String s = "host/#/create?" + signReduce + "&sign=" + sign;
         System.out.println(s);
     }
 
@@ -61,14 +65,14 @@ public class JavaDemo {
      * 查询订单
      */
     public static void queryOrder() {
-        String host = HOST + "/query";
+        String host = "queryHost/open/order/query";
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("orderNo", "1581829465");
-        jsonObject.put("merchantNo", "20200113185052721173545318");
+        jsonObject.put("merchantNo", MERCHANT_NO);
         jsonObject.put("ts", Instant.now().getEpochSecond());
 
-        String signReduce = generateSignReduce(jsonObject, "06f231e8483243e282962294da4ff9ca");
-        String sign = encodeMD5(signReduce);
+        String signReduce = generateSignReduce(jsonObject);
+        String sign = encodeMD5(signReduce + "&key=" + KEY);
         jsonObject.put("sign", sign);
 
         String s = jsonPost(jsonObject.toJSONString(), host);
@@ -79,16 +83,16 @@ public class JavaDemo {
      * 回调
      */
     public static void callback() {
-        String result = "{\"amount\":900,\"bankName\":\"广发银行\",\"bankNo\":\"62146202210026980\",\"merchantNo\":\"20200113185052721173545318\",\"name\":\"王五\",\"orderNo\":\"o-10086140\",\"orderStatus\":50,\"payMode\":\"ebank\",\"payNo\":\"20200213173023981153464943\",\"payStatus\":30,\"payTime\":1581586702,\"sign\":\"3aff08ebde950423acbc267e363588ec\",\"ts\":1581585888}";
+        String result = "{\"amount\":900,\"bankName\":\"广发银行\",\"bankNo\":\"62146202210026980\",\"merchantNo\":\"123456789\",\"name\":\"王五\",\"orderNo\":\"o-10086140\",\"orderStatus\":50,\"payMode\":\"ebank\",\"payNo\":\"20200213173023981153464943\",\"payStatus\":30,\"payTime\":1581586702,\"sign\":\"3aff08ebde950423acbc267e363588ec\",\"ts\":1581585888}";
         JSONObject jsonObject = JSON.parseObject(result);
         //拼接
-        String reduce = generateSignReduce(jsonObject, KEY);
-        String md5 = encodeMD5(reduce);
+        String reduce = generateSignReduce(jsonObject);
+        String md5 = encodeMD5(reduce + "&key=" + KEY);
         System.out.println(jsonObject.getString("sign").equalsIgnoreCase(md5));
     }
 
 
-    public static String generateSignReduce(Object o, String apiKey) {
+    public static String generateSignReduce(Object o) {
         JSONObject parse = JSONObject.parseObject(JSONObject.toJSONString(o));
         String result = parse.keySet()
                 .stream().filter(key -> !"sign".equalsIgnoreCase(key))
@@ -101,7 +105,7 @@ public class JavaDemo {
                     }
                 })
                 .reduce("", (a, b) -> a + "&" + b).substring(1);
-        return result + "&key=" + apiKey;
+        return result;
     }
 
 
